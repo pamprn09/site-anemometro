c599097129b8e7e04b794b11acca9a86b21ad7be
Pamela Ribeiro <contato@pamelanascimento.com>
2024-09-25 14:36:34 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -6,20 +6,28 @@ const ws = new WebSocket('ws://localhost:3000');
 
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
-    const windSpeed = data.windSpeed;
+    console.log(event);
+    
+    const windSpeed = parseFloat(data.windSpeed);
 
-    // Atualizar a velocidade do vento no HTML
-    windSpeedElement.textContent = windSpeed.toFixed(2);
+    // Verificar se windSpeed é um número válido
+    if (!isNaN(windSpeed)) {
+        // Atualizar a velocidade do vento no HTML
+        windSpeedElement.textContent = windSpeed.toFixed(2);
 
-    // Ajustar a rotação da hélice
-    blade.style.animationDuration = (10 / (windSpeed + 1)) + 's';
+        // Ajustar a rotação da hélice
+        blade.style.animationDuration = (10 / (windSpeed + 1)) + 's';
 
-    // Ajustar a cor da hélice de acordo com a velocidade do vento
-    if (windSpeed < 3) {
-        blade.style.backgroundColor = 'blue';
-    } else if (windSpeed < 7) {
-        blade.style.backgroundColor = 'yellow';
+        // Ajustar a cor da hélice de acordo com a velocidade do vento
+        if (windSpeed < 3) {
+            blade.style.backgroundColor = 'blue';
+        } else if (windSpeed < 7) {
+            blade.style.backgroundColor = 'yellow';
+        } else {
+            blade.style.backgroundColor = 'red';
+        }
     } else {
-        blade.style.backgroundColor = 'red';
+        console.error('Valor inválido de windSpeed:', windSpeed);
+        windSpeedElement.textContent = 'Dados inválidos';
     }
 };
